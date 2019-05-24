6da8eb8aad76ce34827ce3336be95e323804dd9d
csijh <csijh@bristol.ac.uk>
2019-05-24 11:40:11 +0100

    interpreter

--- docs/calculator.c
+++ docs/calculator.c
@@ -9,8 +9,8 @@ typedef unsigned char byte;
 // An extended opcode EXTEND+OP has a two-byte, big-endian, unsigned argument.
 enum op {
     STOP, OR, AND, MAYBE, OPT, MANY, DO, LOOK, TRY, HAS, NOT,
-    RULE, START, GO, BACK, EITHER, BOTH, CHAR, SET, STRING, LOW, HIGH, LT,
-    CAT, TAG, MARK, ACT,
+    RULE, START, GO, BACK, EITHER, BOTH, CHAR, SET, SET2, SET3, SET4, STRING,
+    LOW, HIGH, LESS, CAT, TAG, MARK, ACT,
     EXTEND
 };
 
@@ -21,55 +21,58 @@ static byte code[] = {
     START, 8, LOW, 1, 48, HIGH, 1, 57, ACT, number, STOP
 };
 
-// Carry out a (delayed) action, returning the updated value of 'out'.
+// Carry out a (delayed) action, returning the updated value of 'val'.
 static inline
-int doAction(int op, byte input[], int start, int end, int output[], int out) {
+int doAction(int op, byte input[], int start, int end, int values[], int val) {
     if (op == number) {
         int n = 0;
         for (int i = start; i < end; i++) n = n * 10 + (input[i] - '0');
-        output[out++] = n;
+        values[val++] = n;
     }
-    return out;
+    return val;
 }
 
-/// Define a jump table of functions, one for each opcode.
-static instruction *table[] = {
-    [STOP]=doStop, [OR]=doOr, [AND]=doAnd, [MAYBE]=doMaybe, [OPT]=doOpt, /*,
-    [MANY], [DO],
-    [LOOK], [TRY], [HAS], [NOT],
-    [RULE], [START], [GO], [BACK], [EITHER], [BOTH], [CHAR], [SET], [STRING], [RANGE1], [RANGE2], [LT],
-    [CAT], [TAG], [MARK], [ACT]*/
-};
+// Find the length of a UTF-8 character.
+static inline int length(byte first) {
+    if ((first & 0x80) == 0) return 1;
+    if ((first & 0xE0) == 0xC0) return 2;
+    if ((first & 0xF0) == 0xE0) return 3;
+    return 4;
+}
 
 // Parse an input string, producing an integer. Each time round the main loop,
 // carry out any delayed actions then execute an opcode.
-int parse(byte *input) {
+int parse(byte input[]) {
 
     // The parsing state.
-    int stack[1000], delayed[1000], output[1000];
-    int pc, top, start, in, out, delay, look, mark, markers, saveIn, saveDelay;
+    int output[1000], stack[1000], values[1000];
+    int pc, top, start, in, val, out, look, mark, markers, saveIn, saveOut;
     bool ok, act, end;
 
     while(! end) {
 
-        // Carry out any delayed actions, requested by the previous opcode
-        // setting the act flag. For each delayed action, the input position at
-        // the time of recording is stored.
+        // Carry out any delayed actions. An opcode requests this by setting the
+        // act flag, rather than doing it directly, so that this code appears
+        // only once. For each delayed action, the input position at the time of
+        // recording is stored.
         if (act) {
             act = false;
-            for (int i = 0; i < delay; i++) {
-                int op = delayed[i++];
-                int oldIn = delayed[i];
-                out = doAction(op, input, start, oldIn, output, out);
+            for (int i = 0; i < out; i++) {
+                int op = output[i++];
+                int oldIn = output[i];
+                val = doAction(op, input, start, oldIn, values, val);
                 start = oldIn;
             }
-            delay = 0;
+            out = 0;
         }
 
-        // At end, check success or failure and return.
+        // At the end of parsing, check success or failure and return. The STOP
+        // action requests this by setting the end flag, rather than doing it
+        // directly, in case there are outstanding actions to perform using the
+        // above code.
         // TODO report error markers better.
         if (end) {
-            if (ok) return output[0];
+            if (ok) return values[0];
             if (in > mark) markers = 0;
             printf("Parsing failed %x\n", markers);
             exit(1);
@@ -91,7 +94,7 @@ int parse(byte *input) {
 
             // id = x  Entry point. Prepare, continue with x, returning to STOP.
             case START:
-                pc = start = in = out = saveIn = saveOut = 0;
+                pc = start = in = val = saveIn = saveOut = 0;
                 top = look = mark = markers = 0;
                 ok = act = end = false;
                 stack[top++] = pc + arg;
@@ -99,7 +102,7 @@ int parse(byte *input) {
 
             // id = x  After x, tidy up and arrange to return.
             case STOP:
-                if (ok && delay > 0) act = true;
+                if (ok && out > 0) act = true;
                 end = true;
                 break;
 
@@ -116,16 +119,16 @@ int parse(byte *input) {
             // x / y  Save current state, call x, returning to OR.
             case EITHER:
                 stack[top++] = in;
-                stack[top++] = delay;
+                stack[top++] = out;
                 stack[top++] = pc + arg;
                 break;
 
             // x / y  After x, return or continue with y.
             case OR:
-                saveDelay = stack[--top];
+                saveOut = stack[--top];
                 saveIn = stack[--top];
                 if (ok || in > saveIn) pc = stack[--top];
-                else delay = saveDelay;
+                else out = saveOut;
                 break;
 
             // x y  Continue with x, returning to AND.
@@ -141,17 +144,17 @@ int parse(byte *input) {
             // x?, x*  Save state and jump to x, returning to OPT or MANY.
             case MAYBE:
                 stack[top++] = in;
-                stack[top++] = delay;
+                stack[top++] = out;
                 stack[top++] = pc + arg;
                 pc++;
                 break;
 
             // x?  After x, check success and return.
             case OPT:
-                saveDelay = stack[--top];
+                saveOut = stack[--top];
                 saveIn = stack[--top];
-                if (!ok && in == saveIn) {
-                    delay = saveDelay;
+                if (! ok && in == saveIn) {
+                    out = saveOut;
                     ok = true;
                 }
                 pc = stack[--top];
@@ -159,15 +162,15 @@ int parse(byte *input) {
 
             // x*: after x, check success and re-try x or return.
             case MANY:
+                saveOut = stack[--top];
+                saveIn = stack[--top];
                 if (ok) {
-                    stack[top-2] = in;
-                    stack[top-1] = delay;
+                    stack[top++] = in;
+                    stack[top++] = out;
                 }
                 else {
-                    saveDelay = stack[--top];
-                    saveIn = stack[--top];
-                    if (!ok && in == saveIn) {
-                        delay = saveDelay;
+                    if (! ok && in == saveIn) {
+                        out = saveOut;
                         ok = true;
                     }
                     pc = stack[--top];
@@ -180,42 +183,38 @@ int parse(byte *input) {
                 pc = pc + 3;
                 break;
 
-
-        }
-
-    }
-}
-
-/*
             // [x], x&, x!: start a lookahead.
             case LOOK:
-                saveIn = in;
-                saveOut = out;
+                stack[top++] = in;
+                stack[top++] = out;
                 look++;
                 stack[top++] = pc;
                 pc = pc + 1;
                 break;
 
-            // x&: after x, backtrack and report success.
+            // x&: after x, backtrack and return.
             case HAS:
+                out = stack[--top];
+                in = stack[--top];
                 look--;
-                out = saveOut;
-                in = saveIn;
+                pc = stack[--top];
                 break;
 
-            // x!: after x, backtrack and report inverted result.
+            // x!: after x, backtrack, inverted the result, and return.
             case NOT:
+                out = stack[--top];
+                in = stack[--top];
                 look--;
-                out = saveOut;
-                in = saveIn;
                 ok = ! ok;
+                pc = stack[--top];
                 break;
 
             // [x]: succeed and perform actions, or fail and discard them.
             case TRY:
+                saveOut = stack[--top];
+                in = stack[--top];
                 look--;
-                in = saveIn;
-                if (ok && look == 0) doActions(output, saveOut, out);
+                if (ok && look == 0) act = true;
                 else out = saveOut;
                 break;
 
@@ -223,27 +222,28 @@ int parse(byte *input) {
             case CHAR:
                 ok = (input[in] == arg);
                 if (ok) {
-                    if (look == 0 && out > saveOut) {
-                        doActions(output, saveOut, out);
-                    }
+                    if (look == 0 && out > saveOut) act = true;
                     in++;
                 }
+                pc = stack[--top];
                 break;
 
             // 'abc': check for each character in a set.
-            // TODO: support UTF-8
-            case SET:
+            case SET: case SET2: case SET3: case SET4:
                 ok = false;
-                for (int i = 0; i < arg && !ok; i++) {
-                    assert(input[in+i] < 128);
-                    if (input[in+i] == code[pc+i]) ok = true;
+                int k = 1 + (op - SET);
+                for (int i = 0; i < arg && ! ok; i = i + k) {
+                    bool okch = true;
+                    for (int j = 0; j < k; j++) {
+                        if (input[in+i+j] != code[pc+i+j]) okch = false;
+                    }
+                    if (okch) ok = true;
                 }
                 if (ok) {
-                    in++;
-                    if (look == 0 && out > saveOut) {
-                        doActions(output, saveOut, out);
-                    }
+                    if (look == 0 && out > saveOut) act = true;
+                    in = in + k;
                 }
+                pc = stack[--top];
                 break;
 
             // "abc": check for string and return
@@ -256,13 +256,13 @@ int parse(byte *input) {
                 }
                 if (ok) {
                     in = in + arg;
-                    if (look == 0 && out > saveOut) doActions();
+                    if (look == 0 && out > saveOut) act = true;
                 }
                 pc = stack[--top];
                 break;
 
-            // "a".."z": check >= "a", return or continue with RANGE2
-            case RANGE1:
+            // 'a'..'z': check >= 'a', return or continue with HIGH
+            case LOW:
                 ok = true;
                 for (int i = 0; i < arg; i++) {
                     byte ch = input[in+i];
@@ -273,23 +273,24 @@ int parse(byte *input) {
                 else pc = stack[--top];
                 break;
 
-            // "a".."z": check second half of range and return.
-            case RANGE2:
+            // 'a'..'z': check <= 'z' and return.
+            case HIGH:
                 ok = true;
                 for (int i = 0; i < arg; i++) {
                     byte ch = input[in+i];
                     if (ch == '\0') break;
                     if (ch > code[pc+i]) ok = false;
                 }
+                if (input[in] == '\0') ok = false;
                 if (ok) {
-                    in = in + arg; // TODO check!
-                    if (look == 0 && out > saveOut) doActions();
+                    in = in + length(input[in]);
+                    if (look == 0 && out > saveOut) act = true;
                 }
                 pc = stack[--top];
                 break;
 
             // <a>: check if input < "a", return
-            case LT:
+            case LESS:
                 ok = true;
                 for (int i = 0; i < arg; i++) {
                     byte ch = input[in+i];
@@ -299,17 +300,16 @@ int parse(byte *input) {
                 pc = stack[--top];
                 break;
 
-            // Categories are not implemented and other tags are not expected.
-            case CAT: case TAG: case ACT1: case ACT2: case ACT3: case ACTN:
+            // Categories and tags are not implemented.
+            case CAT: case TAG:
                 printf("Opcode %d not implemented\n", op);
                 exit(1);
                 break;
 
-            // Record an error marker.
+            // Record an error marker. Assume arg < 32.
             case MARK:
-                assert(arg < 32);
                 if (look = 0) {
-                    if (mark != in) {
+                    if (in > mark) {
                         mark = in;
                         markers = 0;
                     }
@@ -321,172 +321,11 @@ int parse(byte *input) {
 
             // Delay any action and return success.
             case ACT:
-                output[out++] = (in << 24) | arg;
+                output[out++] = in;
+                output[out++] = arg;
                 ok = true;
                 pc = stack[--top];
                 break;
         }
     }
 }
-*/
-/*
-
-int saveIn, saveOut, saveMark, length;
-String text;
-if (tracing && ! skipTrace) System.out.println(node.trace());
-skipTrace = false;
-switch(node.op()) {
-case SOME:
-    saveIn = in;
-    saveOut = out;
-    parse(node.left());
-    if (!ok && in == saveIn) out = saveOut;
-    if (!ok) return;
-    saveIn = in;
-    saveOut = out;
-    while (ok) {
-        saveIn = in;
-        saveOut = out;
-        parse(node.left());
-    }
-    if (!ok && in == saveIn) {
-        out = saveOut;
-        ok = true;
-    }
-    break;
-case TRY:
-    saveIn = in;
-    saveOut = out;
-    lookahead++;
-    parse(node.left());
-    lookahead--;
-    in = saveIn;
-    if (ok && lookahead == 0) takeActions();
-    if (ok) parse(node.left());
-    else out = saveOut;
-    break;
-case HAS:
-    saveIn = in;
-    saveOut = out;
-    lookahead++;
-    parse(node.left());
-    lookahead--;
-    out = saveOut;
-    in = saveIn;
-    break;
-case NOT:
-    saveIn = in;
-    saveOut = out;
-    lookahead++;
-    parse(node.left());
-    lookahead--;
-    out = saveOut;
-    in = saveIn;
-    ok = !ok;
-    break;
-case CHAR:
-    if (in >= input.length()) ok = false;
-    else {
-        int ch = input.codePointAt(in);
-        ok = (ch == node.value());
-        if (ok) {
-            if (lookahead == 0 && out > 0) takeActions();
-            int n = Character.charCount(ch);
-            in += n;
-            if (tracing) traceInput();
-        }
-    }
-    break;
-case STRING:
-    length = node.text().length() - 2;
-    text = node.text().substring(1, length+1);
-    ok = true;
-    if (in + length > input.length()) ok = false;
-    else for (int i=0; i<length; i++) {
-        if (input.charAt(in+i) != text.charAt(i)) { ok = false; break; }
-    }
-    if (ok) {
-        if (lookahead == 0 && out > 0) takeActions();
-        in += length;
-        if (tracing) traceInput();
-    }
-    break;
-case SET:
-    length = node.text().length() - 2;
-    text = node.text().substring(1, length+1);
-    ok = false;
-    if (in >= input.length()) { }
-    else for (int i=0; i<length; i++) {
-        if (input.charAt(in) != text.charAt(i)) continue;
-        if (lookahead == 0 && out > 0) takeActions();
-        if (Character.isHighSurrogate(text.charAt(i))) {
-            i++;
-            if (input.charAt(in+1) != text.charAt(i)) continue;
-            in++;
-        }
-        in++;
-        if (tracing) traceInput();
-        ok = true;
-    }
-    break;
-case RANGE:
-    int low = node.left().value();
-    int high = node.right().value();
-    ok = false;
-    if (in < input.length()) {
-        int ch = input.codePointAt(in);
-        ok = (ch >= low) && (ch <= high);
-        if (ok) {
-            if (lookahead == 0 && out > 0) takeActions();
-            int n = Character.charCount(ch);
-            in += n;
-            if (tracing) traceInput();
-        }
-    }
-    break;
-case CAT:
-    ok = false;
-    int cats = node.value();
-    if (in < input.length()) {
-        int ch = input.codePointAt(in);
-        int bit = 1 << Character.getType(ch);
-        ok = ((cats & bit) != 0);
-        if (ok) {
-            if (lookahead == 0 && out > 0) takeActions();
-            int n = Character.charCount(ch);
-            in += n;
-            if (tracing) traceInput();
-        }
-    }
-    break;
-case TAG:
-    String query;
-    if (node.text().charAt(0) == '%') query = node.text().substring(1);
-    else query = node.text().substring(1, node.text().length()-1);
-    if (query.length() == 0) ok = in == input.length();
-    else ok = input.startsWith(query, in);
-    if (ok) {
-        start = in;
-        if (lookahead == 0 && out > 0) takeActions();
-        in += query.length();
-        while (in < input.length() &&
-            (input.charAt(in) == ' ' || input.charAt(in) == '\n')) in++;
-        if (tracing) traceInput();
-    }
-    break;
-case MARK:
-    ok = true;
-    if (lookahead > 0) break;
-    if (mark != in) { mark = in; failures.clear(); }
-    failures.set(node.value());
-    break;
-case DROP:
-    ok = true;
-    delay[out++] = node;
-    break;
-case ACT:
-    ok = true;
-    if (lookahead > 0) break;
-    delay[out++] = node;
-    break;
-*/
