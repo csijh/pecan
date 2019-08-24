c3e1458864bce5940d144992181bef99d30f17e6
Ian Holyer <csijh@bristol.ac.uk>
2019-08-24 22:56:28 +0100

    more work on C interpreter

--- docs/calculator.c
+++ docs/calculator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "interpreter.h"
+#include "parser.h"
 
 // Action constants.
 enum action { number = 0 };
@@ -47,104 +47,13 @@ int parse(byte input[], act) {
         switch(op) {
 
 
-            // 'abc': check for each character in a set.
-            case SET: case SET2: case SET3: case SET4:
-                ok = false;
-                int k = 1 + (op - SET);
-                for (int i = 0; i < arg && ! ok; i = i + k) {
-                    bool okch = true;
-                    for (int j = 0; j < k; j++) {
-                        if (input[in+i+j] != code[pc+i+j]) okch = false;
-                    }
-                    if (okch) ok = true;
-                }
-                if (ok) {
-                    if (look == 0 && out > saveOut) act = true;
-                    in = in + k;
-                }
-                pc = stack[--top];
-                break;
-
-            // "abc": check for string and return
-            case STRING:
-                ok = true;
-                for (int i = 0; i < arg; i++) {
-                    byte ch = input[in+i];
-                    if (ch != code[pc+i]) ok = false;
-                    if (ch == '\0') break;
-                }
-                if (ok) {
-                    in = in + arg;
-                    if (look == 0 && out > saveOut) act = true;
-                }
-                pc = stack[--top];
-                break;
-
-            // 'a'..'z': check >= 'a', return or continue with HIGH
-            case LOW:
-                ok = true;
-                for (int i = 0; i < arg; i++) {
-                    byte ch = input[in+i];
-                    if (ch < code[pc+i]) ok = false;
-                    if (ch == '\0') break;
-                }
-                if (ok) pc = pc + arg;
-                else pc = stack[--top];
-                break;
-
-            // 'a'..'z': check <= 'z' and return.
-            case HIGH:
-                ok = true;
-                for (int i = 0; i < arg; i++) {
-                    byte ch = input[in+i];
-                    if (ch == '\0') break;
-                    if (ch > code[pc+i]) ok = false;
-                }
-                if (input[in] == '\0') ok = false;
-                if (ok) {
-                    in = in + length(input[in]);
-                    if (look == 0 && out > saveOut) act = true;
-                }
-                pc = stack[--top];
-                break;
-
-            // <a>: check if input < "a", return
-            case LESS:
-                ok = true;
-                for (int i = 0; i < arg; i++) {
-                    byte ch = input[in+i];
-                    if (ch == '\0') break;
-                    if (ch >= code[pc+i]) ok = false;
-                }
-                pc = stack[--top];
-                break;
-
             // Categories and tags are not implemented.
             case CAT: case TAG:
                 printf("Opcode %d not implemented\n", op);
                 exit(1);
                 break;
 
-            // Record an error marker. Assume arg < 32.
-            case MARK:
-                if (look = 0) {
-                    if (in > mark) {
-                        mark = in;
-                        markers = 0;
-                    }
-                    markers = markers | (1 << arg);
-                }
-                ok = true;
-                pc = stack[--top];
-                break;
 
-            // Delay any action and return success.
-            case ACT:
-                output[out++] = in;
-                output[out++] = arg;
-                ok = true;
-                pc = stack[--top];
-                break;
         }
     }
 }
